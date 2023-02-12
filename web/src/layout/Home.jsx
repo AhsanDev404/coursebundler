404596dc38b95eb5e928287220293261753bc9f1
AhsanDev404 <ahsan.dev.404@gmail.com>
2023-02-12 23:16:14 +0500

    home page

--- web/src/layout/Home.jsx
+++ web/src/layout/Home.jsx
@@ -1,9 +1,106 @@
-import React from 'react'
-
+import React from 'react';
+import {
+  Box,
+  Button,
+  color,
+  Heading,
+  HStack,
+  Image,
+  Stack,
+  Text,
+  VStack,
+} from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
+import bg from '../assets/images/bg.png';
+import '../styles/home.css';
+import { CgGoogle, CgYoutube } from 'react-icons/cg';
+import { SiCoursera, SiUdemy } from 'react-icons/si';
+import { DiAws } from 'react-icons/di';
+import intro from '../assets/videos/intro.mp4';
 function Home() {
   return (
-    <div>Home</div>
-  )
+    <Box>
+      <Stack
+        height={window.innerHeight}
+        direction={['column', 'row']}
+        alignItems="center"
+        margin={5}
+      >
+        <VStack
+          w={'100%'}
+          alignItems={['center', 'flex-end']}
+          marginX={2}
+          gap={2}
+        >
+          <Heading
+            as={'h1'}
+            textTransform={'uppercase'}
+            children="learn from the experts"
+            size={'2xl'}
+            textAlign="center"
+          />
+          <Text
+            textTransform={'capitalize'}
+            children="find valuable content in reasonable price"
+          />
+          <Link to={'/course'}>
+            <Button
+              textTransform={'capitalize'}
+              bg={'yellow.400'}
+              color="black"
+            >
+              explore now
+            </Button>
+          </Link>
+        </VStack>
+
+        <Image
+          className="vector-graphics"
+          src={bg}
+          boxSize="md"
+          objectFit="contain"
+        />
+      </Stack>
+
+      <Box bg={'blackAlpha.800'} padding={2}>
+        <Heading
+          textTransform={'uppercase'}
+          textAlign="center"
+          color={'yellow.400'}
+          children="our brands"
+        />
+        <HStack
+          justifyContent={'space-around'}
+          color={'white'}
+          fontSize="3rem"
+          className="brandBanner"
+          marginY={4}
+        >
+          <CgGoogle />
+          <CgYoutube />
+          <SiCoursera />
+          <SiUdemy />
+          <DiAws />
+        </HStack>
+      </Box>
+
+      <HStack
+        justifyContent={'center'}
+        className="video-container"
+        marginY={10}
+      >
+        <Box width={['100%', '60%']}>
+          <video  
+            controls
+            src={intro}
+            disablePictureInPicture
+            disableRemotePlayback
+            controlsList="nodownload nofullscreen noremoteplayback"
+          />
+        </Box>
+      </HStack>
+    </Box>
+  );
 }
 
-export default Home
+export default Home;
