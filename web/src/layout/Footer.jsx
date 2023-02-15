12ac441231e52622f7709b6399751a1cc991b892
AhsanDev404 <ahsan.dev.404@gmail.com>
2023-02-16 02:35:43 +0500

    footer complete

---
+++ web/src/layout/Footer.jsx
@@ -0,0 +1,29 @@
+import { Box, Heading, HStack, Link, Stack, Text, VStack } from '@chakra-ui/react'
+import React from 'react'
+import {DiGithub} from 'react-icons/di'
+import {TiSocialLinkedinCircular} from 'react-icons/ti'
+function Footer() {
+  return (
+    <Box bg={'blackAlpha.900'} color='white' padding={5}>
+        <Stack direction={['column' , 'row']} justifyContent='space-between' >
+            <VStack alignItems={'flex-start'}>
+                <Heading children = 'All Rights Reserved' />
+                <Heading size={'sm'} color={'yellow.400'} children='@ AhsanDev404'/>
+            </VStack>
+            <HStack fontSize={'5xl'}>
+                <Link href='https://github.com/AhsanDev404' target={'_blank'}>
+                <DiGithub />
+                </Link>
+                <Link href='https://www.linkedin.com/in/muhammad-ahsan-526271261/' target={'_blank'}>
+                <TiSocialLinkedinCircular />
+                </Link>
+                
+            </HStack>
+
+        </Stack>
+        
+    </Box>
+  )
+}
+
+export default Footer
