0d476801173a3b6dd2b53a5d19daca7dc695a167
AhsanDev404 <ahsan.dev.404@gmail.com>
2023-02-16 00:50:58 +0500

    course page

--- web/src/layout/Courses.jsx
+++ web/src/layout/Courses.jsx
@@ -1,12 +1,71 @@
-import { Box } from '@chakra-ui/react'
-import React from 'react'
+import {
+  Box,
+  Button,
+  Container,
+  Grid,
+  Heading,
+  HStack,
+  Input,
+  Stack,
+  Text,
+  VStack,
+} from '@chakra-ui/react';
+import React, { useState } from 'react';
+import CourseCard from '../components/CourseCard';
 
 function Courses() {
+  const [keyword, setKeyWord] = useState('');
+  const category = [
+    'Data Structure',
+    'Algorithms',
+    'Web Development',
+    'Mobile App Development',
+    'React',
+    'Java Script',
+    'HTML',
+    'CSS',
+    'NEXT.js',
+  ];
   return (
-    <Box>
-        Courses
-    </Box>
-  )
+    <Container maxW={'container.lg'} paddingY={'8'}>
+      <Heading children="All Courses" m={8} />
+      <Input
+        value={keyword}
+        onChange={e => {
+          setKeyWord(e.target.value);
+        }}
+        type="text"
+        placeholder="search a course..."
+        focusBorderColor="yellow.500"
+      />
+      <HStack
+        overflowX={'auto'}
+        paddingY={8}
+        css={{ '&::-webkit-scrollbar': { display: 'none' } }}
+      >
+        {category.map((item, index) => (
+          <Button key={index} minW={'60'}>
+            <Text children={item} />
+          </Button>
+        ))}
+      </HStack>
+      <Stack
+        direction={['column', 'row']}
+        flexWrap="wrap"
+        rowGap={5}
+        columnGap={5}
+        alignItems="center"
+      >
+        <CourseCard
+          src="https://images.pexels.com/photos/7364280/pexels-photo-7364280.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+          title=" default title of the course"
+          description="default description of the course goes here"
+          createdBy="default user name"
+          lecturesCount="00"
+        />
+      </Stack>
+    </Container>
+  );
 }
 
-export default Courses
+export default Courses;
