ef4e155bbadffea5f5bd2ee24e505bead6ed500d
Muhammad Ahsan <ahsan.dev.404@gmail.com>
2023-03-02 15:50:44 +0500

    custom error handler

--- server/app.js
+++ server/app.js
@@ -1,10 +1,13 @@
 import express from "express";
 import courseRoute from "./src/routes/CourseRoute.js";
 import userRoute from "./src/routes/userRoute.js";
+import ErrorMiddleware from "./src/middlewares/Error.js";
 const app = express();
 
 
 app.use('/api', courseRoute)
 app.use('/api', userRoute)
+
+app.use(ErrorMiddleware)
 
 export default app;
--- server/src/controllers/courseController.js
+++ server/src/controllers/courseController.js
@@ -1,9 +1,10 @@
+import { catchAsyncError } from "../middlewares/catchAsyncError.js";
 import Course from "../models/courseModel.js";
 
-export const getAllCourse = async(req,res,next)=>{
+export const getAllCourse = catchAsyncError(async(req,res,next)=>{
     const course = await Course.find()
     res.status(200).json({
         success:true,
         course
     })
-}
+})
