67c25e2e83f1b0a781e50f4212c255369dde02e5
Muhammad Ahsan <ahsan.dev.404@gmail.com>
2023-03-03 13:21:13 +0500

    create course route

--- server/app.js
+++ server/app.js
@@ -4,6 +4,12 @@ import userRoute from "./src/routes/userRoute.js";
 import ErrorMiddleware from "./src/middlewares/Error.js";
 const app = express();
 
+app.use(express.json())
+app.use(
+    express.urlencoded({
+        extended:true,
+    })
+)
 
 app.use('/api', courseRoute)
 app.use('/api', userRoute)
