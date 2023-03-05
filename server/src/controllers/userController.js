6a391651849b26ad6d95f87af281bc882a1b0a08
Muhammad Ahsan <ahsan.dev.404@gmail.com>
2023-03-05 21:06:49 +0500

    login + logout

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -30,8 +30,35 @@ export const register = catchAsyncError(async(req,res,next)=>{
     sendToken(res,user,"Register Successfully",201)
 })
 
-export const login  = catchAsyncError((req,res,next)=>{
+export const login  = catchAsyncError(async(req,res,next)=>{
     const {email , password} = req.body
 
+    if(!email || !password){
+        return(next(new Errorhandler('Please Enter Email and Password' , 400)))
+    }
 
+    const user = await User.findOne({email}).select("+password")
+
+    if(!user){
+        return(next(new Errorhandler('User Not exist' , 401)))
+    }
+
+    const isMatched = await user.comparePassword(password)
+
+    if(!isMatched){
+        return(next(new Errorhandler('Wrong Email Password' , 401)))
+    }
+
+    sendToken(res,user,`Welcome Back ${user.name}`,200)
+
+
+})
+
+export const logout = catchAsyncError(async(req,res,next)=>{
+    res.status(200).cookie("token" , null , {
+        expires: new Date(Date.now())
+    }).json({
+        success:true,
+        message:'Logout Successfully'
+    })
 })
--- server/src/routes/userRoute.js
+++ server/src/routes/userRoute.js
@@ -1,8 +1,11 @@
 import express from 'express'
-import { register } from '../controllers/userController.js'
+import { login, logout, register } from '../controllers/userController.js'
 
 const userRoute = express.Router()
 
 userRoute.route('/register').post(register)
+
+userRoute.route('/login').post(login)
+userRoute.route('/logout').get(logout)
 
 export default userRoute 
